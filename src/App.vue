159851977317765676559bb71fd78faab797a318
Charlie <meow@charliecat.space>
2023-05-15 22:35:21 +0200

    refactor(App.vue): Add Emojis as icons of the pages

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,7 @@ import { ref, provide } from 'vue';
 
 const StuffToDo = ref([
   {  $type: 'todo-v1', $id: 'todo-a1-0', title: "Otestuj ma!",                                                      done: false, },
-  {  $type: 'todo-v1', $id: 'todo-a1-1', title: "sprav si novy vue projekt",                                        done: true,  },
-  {  $type: 'todo-v1', $id: 'todo-a1-2', title: "daj si tam input na novu todo polozku",                            done: true,  },
-  {  $type: 'todo-v1', $id: 'todo-a1-3', title: "input si hookni na click, aby pridal do zoznamu v data novy item", done: true,  },
-  {  $type: 'todo-v1', $id: 'todo-a1-4', title: "zobraz itemy",                                                     done: true,  },
-  {  $type: 'todo-v1', $id: 'todo-a1-5', title: "itemom pridaj deleted atribut",                                    done: true,  },
-  {  $type: 'todo-v1', $id: 'todo-a1-6', title: "ked je deleted, tak cez v-if sa schova",                           done: true,  },
-  {  $type: 'todo-v1', $id: 'todo-a1-7', title: "sprav druhu pagu kde budu zmazane veci",                           done: true,  },
-])
+  ])
 
 const InputArea = ref(null)
 
@@ -45,9 +38,9 @@ provide('UpdateToDo', UpdateToDo);
     </div>
     <div class="flex-auto text-right">
       <nav>
-        <RouterLink to="/">Stuff To-Do</RouterLink>
+        <RouterLink to="/"> Stuff To-Do</RouterLink>
         •
-        <RouterLink to="/deleted">Stuff Done</RouterLink>
+        <RouterLink to="/deleted">✔ Stuff Done</RouterLink>
         •
         <a href="https://github.com/0Charliecat/acad-todo" class="underline inline" title="Icon from Octoicons"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="inline mb-1" width="16" height="16"><path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path></svg></a>
       </nav>
