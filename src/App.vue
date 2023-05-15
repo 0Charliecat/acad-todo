62bfb0c23e3059e68b2e127e64f7a23ff6aaea52
Charlie <meow@charliecat.space>
2023-05-15 20:01:43 +0200

    add(App.vue): Create Header, init Manage ToDos
     • ToDos management is brought to you by the awesome provide/inject feature of vue's
     • Header, just Header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { ref, provide } from 'vue';
 
-const StuffToDo = [
+const StuffToDo = ref([
   {  $type: 'todo-v1', $id: 'todo-a1-1', title: "sprav si novy vue projekt",                                        done: true,  },
   {  $type: 'todo-v1', $id: 'todo-a1-2', title: "daj si tam input na novu todo polozku",                            done: false, },
   {  $type: 'todo-v1', $id: 'todo-a1-3', title: "input si hookni na click, aby pridal do zoznamu v data novy item", done: false, },
@@ -9,21 +10,32 @@ const StuffToDo = [
   {  $type: 'todo-v1', $id: 'todo-a1-5', title: "itemom pridaj deleted atribut",                                    done: false, },
   {  $type: 'todo-v1', $id: 'todo-a1-6', title: "ked je deleted, tak cez v-if sa schova",                           done: false, },
   {  $type: 'todo-v1', $id: 'todo-a1-7', title: "sprav druhu pagu kde budu zmazane veci",                           done: false, },
-]
+])
+
+function UpdateToDo(id, method, attr) {
+  if (method === 'remove') {
+    StuffToDo.value.find(e=>e.$id===id)
+  }
+}
+
+provide('ToDos', StuffToDo);
+provide('UpdateToDo', UpdateToDo);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+  <header class="flex m-5 border-b-2 border-b-emerald-400">
+    <div>
+      <h1>☑️ The acad<span class="font-bold">ToDo App</span></h1>
+    </div>
+    <div class="flex-auto text-right">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Stuff To-Do</RouterLink>
+        •
+        <RouterLink to="/deleted">Stuff Done</RouterLink>
       </nav>
     </div>
+    
+
   </header>
 
   <RouterView />
