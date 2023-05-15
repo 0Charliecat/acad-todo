ee106ccbbfb2413ea0dcc11653942c4f93f00565
Charlie <meow@charliecat.space>
2023-05-15 19:05:03 +0200

    add(App.vue): added sample data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+const StuffToDo = [
+  {  $type: 'todo-v1', $id: 'todo-a1-1', title: "sprav si novy vue projekt",                                        done: true,  },
+  {  $type: 'todo-v1', $id: 'todo-a1-2', title: "daj si tam input na novu todo polozku",                            done: false, },
+  {  $type: 'todo-v1', $id: 'todo-a1-3', title: "input si hookni na click, aby pridal do zoznamu v data novy item", done: false, },
+  {  $type: 'todo-v1', $id: 'todo-a1-4', title: "zobraz itemy",                                                     done: false, },
+  {  $type: 'todo-v1', $id: 'todo-a1-5', title: "itemom pridaj deleted atribut",                                    done: false, },
+  {  $type: 'todo-v1', $id: 'todo-a1-6', title: "ked je deleted, tak cez v-if sa schova",                           done: false, },
+  {  $type: 'todo-v1', $id: 'todo-a1-7', title: "sprav druhu pagu kde budu zmazane veci",                           done: false, },
+]
 </script>
 
 <template>
@@ -19,4 +28,3 @@ import HelloWorld from './components/HelloWorld.vue'
 
   <RouterView />
 </template>
-
