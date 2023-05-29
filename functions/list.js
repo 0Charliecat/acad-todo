87a44d05905269df65a03ac6ca2dfab358b2ed59
Charlie <meow@charliecat.space>
2023-05-29 20:52:39 +0200

    Refactor the "list.js" function to properly return JSON objects
    This commit refactors the "list.js" function to return JSON objects by using the "JSON.stringify()" method and adding a "Content-type" header to the response. The update ensures that the function returns well-formed JSON responses that will be correctly interpreted by client applications.

--- functions/list.js
+++ functions/list.js
@@ -1,7 +1,13 @@
 export function onRequestGet() {
-    return new Response().json([
-        {  $type: 'todo-v1', $id: `init${Date.now()}-1`, title: "Otestuj ma!", done: false, },
-        {  $type: 'todo-v1', $id: `init${Date.now()}-2`, title: "fetch cez CF Functions", done: false, },
-        {  $type: 'todo-v1', $id: `init${Date.now()}-3`, title: "ahoj!", done: false, },
-    ])
+    return new Response(JSON.stringify(
+        [
+            {  $type: 'todo-v1', $id: `init${Date.now()}-1`, title: "Otestuj ma!", done: false, },
+            {  $type: 'todo-v1', $id: `init${Date.now()}-2`, title: "fetch cez CF Functions", done: false, },
+            {  $type: 'todo-v1', $id: `init${Date.now()}-3`, title: "ahoj!", done: false, },
+        ]
+    ), {
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
 }
