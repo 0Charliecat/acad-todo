0aa9aafe5aa602faa5c405633d6fbdb8c6048905
Charlie <meow@charliecat.space>
2023-06-06 18:27:37 +0200

    Update updat func with cors ACA-Headers to `*`

--- functions/update.js
+++ functions/update.js
@@ -1,8 +1,9 @@
-export function onRequest(context) {
+export function onRequestPost(context) {
     return new Response(JSON.stringify({ status: 'ok' }), {
         headers: {
             "Content-type": "application/json",
-            'Access-Control-Allow-Origin': '*'
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': "*"
         }
     })
 }
