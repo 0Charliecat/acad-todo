5ae522d1eb06544ee5e02e13a836f3d00bda8e3a
0CharlieCat <meow@charliecat.space>
2023-07-10 17:13:37 +0000

    commit `src/views/editTodo.vue`

---
+++ src/views/editTodo.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <label for="">What to do?</label>
+    <input type="text" v-model="rqst.title" @change="onTextChange" class="bg-emerald-100 mx-2 px-2 flex-auto rounded-full text-emerald-900">
+
+</template>
+
+<script>
+export default {
+    props: {
+        id: String
+    },
+    data() {
+        console.log(this)
+        return {
+            rqst: this.$store.getters.objected[this.id]
+        }
+    },
+    methods: {
+        onTextChange(e) {
+            console.log(e)
+        }
+    }
+}
+</script>
