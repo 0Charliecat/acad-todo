b7b3c811b0f4c5b78a53530e950fab3cd4441cff
0Charliecat <meow@charliecat.space>
2023-07-17 09:17:49 +0200

    👌 IMPROVE: edittodo using store mutation

--- src/store.js
+++ src/store.js
@@ -1,52 +1,62 @@
-import { createStore } from 'vuex'
+import {
+	createStore
+} from 'vuex'
 // Create a new store instance.
 export default createStore({
-    state () {
-      return {
-        todos: []
-      }
-    },
-    mutations: {
-      populate (state, todos) {
-        state.todos.length = 0
-        todos.forEach(e=> {
-          state.todos.push(e)
-        })
-      },
-      flip (state, id) {
-          state.todos = state.todos.map(e=> { 
-              if (e.$id===id) {
-                e.done = !e.done;
-                //SendUpdatedToDo(e)
-              }
-              return e
-          })
-      },
-      put (state, data) {
-        state.todos.push(data)
-      }
-    },
-    getters: {
-      done (state) {
-        return state.todos.filter(todo => todo.done)
-      },
-      undone (state) {
-          return state.todos.filter(todo => !todo.done)
-      },
-      doneCount (state, getters) {
-          return getters.done.length
-      },
-      undoneCount (state, getters) {
-          return getters.undone.length
-      },
-      count (state) {
-          return state.todos.length
-      },
-      objected (state) {
-        return state.todos.reduce((a, v) => {
-          a[v.$id] = v
-          return a;
-        }, {})
-      },
-    }
+	state() {
+		return {
+			todos: []
+		}
+	},
+	mutations: {
+		populate(state, todos) {
+			state.todos.length = 0
+			todos.forEach(e => {
+				state.todos.push(e)
+			})
+		},
+		flip(state, id) {
+			state.todos = state.todos.map(e => {
+				if (e.$id === id) {
+					e.done = !e.done;
+					//SendUpdatedToDo(e)
+				}
+				return e
+			})
+		},
+		put(state, data) {
+			state.todos.push(data)
+		},
+		edit(state, data) {
+			state.todos = state.todos.map(e => {
+				if (e.$id === data.$id) {
+					e = data;
+				} 
+				return e;
+			})
+		}
+	},
+	getters: {
+		done(state) {
+			return state.todos.filter(todo => todo.done)
+		},
+		undone(state) {
+			return state.todos.filter(todo => !todo.done)
+		},
+		doneCount(state, getters) {
+			return getters.done.length
+		},
+		undoneCount(state, getters) {
+			return getters.undone.length
+		},
+		count(state) {
+			return state.todos.length
+		},
+		objected(state) {
+			return state.todos.reduce((a, v) => {
+				a[v.$id] = v
+				return a;
+			}, {})
+		},
+	}
 })
--- src/views/editTodo.vue
+++ src/views/editTodo.vue
@@ -1,7 +1,7 @@
 <template>
 
     <label for="">What to do?</label>
-    <input type="text" v-model="rqst.title" @change="onTextChange" class="bg-emerald-100 mx-2 px-2 flex-auto rounded-full text-emerald-900">
+    <input type="text" v-model="rqst.title" @input="onTextChange" class="bg-emerald-100 mx-2 px-2 flex-auto rounded-full text-emerald-900">
 
 </template>
 
@@ -13,12 +13,14 @@ export default {
     data() {
         console.log(this)
         return {
-            rqst: this.$store.getters.objected[this.id]
+            rqst: {...this.$store.getters.objected[this.id]}
         }
     },
     methods: {
         onTextChange(e) {
-            console.log(e)
+            let editedToDo = this.rqst;
+            console.log(`ToDo Edit:`, this.id, e, editedToDo)
+            this.$store.commit('edit', editedToDo)
         }
     }
 }
